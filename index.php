dad7e6ed1893ea38df530cba4ca2ccff8fec7799
Ali Zaib <alizaib1217@gmail.com>
2019-07-04 14:27:24 +0500

    updates

--- index.php
+++ index.php
@@ -9,8 +9,49 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="js/jquery-3.2.1.min.jss"></script>
+    <link rel="stylesheet" href="./css/style.css">
+
 </head>
 <body>
-<h1>Welcome to the website</h1>
+<div class="header-wrapper">
+    <img src="./assets/icons/logo.png" class="logo" alt="Logo">
+    <!--    <div class="collapse navbar-collapse" id="navbarSupportedContent">-->
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link active" href="#">HOME</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">ABOUT</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">BLOG</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">SHOP</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">PAGES</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">CONTACT</a>
+        </li>
+    </ul>
+
+    <!--    </div>-->
+    <div class="header-icons">
+        <img src="./assets/icons/person.png" class="person-profile" alt="Profile">
+        <img src="./assets/icons/cart.png" class="my-cart" alt="Cart">
+    </div>
+</div>
+<div class="banner">
+    <img src="assets/images/banner.jpg" class="banner-img">
+    <img src="./assets/icons/logo_1.png" class="banner-logo" alt="Logo">
+</div>
+<div style="position:relative">
+    <img src="assets/images/product1.jpg" alt="Product1" class="productItem">
+
+</div>
+
 </body>
 </html>
